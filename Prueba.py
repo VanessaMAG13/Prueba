c6549adc5f12ce599e201245e8e16545557e5d0b
Vanessa <89747961+VanessaMAG13@users.noreply.github.com>
2023-09-10 22:56:01 -0500

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,10 @@
+with open("uniprotkb_Ecoli.fasta") as file:  # File lecture
+    for i in file:
+        print(i)
+        # i = i.rstrip()
+        # if i.startswith(">"):
+        #     print(i)
+            # temp = i.split()
+            # print(temp,sep="|")
+            # cod = i.split("OS=")
+           # print(cod)
